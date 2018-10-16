185365484c44c9ab29f54ad949b0374d25dc8815
madison-kerndt <madison.kerndt@gmail.com>
2018-10-16 10:46:29 -0600

    Update snackbar styles

--- src/app/components/snackbar/snackbar.component.ts
+++ src/app/components/snackbar/snackbar.component.ts
@@ -2,34 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { AnnouncementService } from '../../services/announcement/announcement.service';
 
 @Component({
-  selector: 'app-snackbar',
-  templateUrl: './snackbar.component.html',
-  styleUrls: ['./snackbar.component.css']
+    selector: 'app-snackbar',
+    templateUrl: './snackbar.component.html',
+    styleUrls: ['./snackbar.component.css']
 })
 export class SnackbarComponent implements OnInit {
-  public message = '';
-  public klass = 'snackbar';
-  private interval;
+    public message = '';
+    public class = 'snackbar';
+    private interval;
 
-  constructor(private announcementService: AnnouncementService) {
-  }
+    constructor(private announcementService: AnnouncementService) {
+    }
 
-  ngOnInit() {
-    this.announcementService.newAnnouncement$.subscribe((a) => {
-      this.message = a.message;
-      this.startTimer();
-    });
-  }
+    ngOnInit() {
+        this.announcementService.newAnnouncement$.subscribe((a) => {
+            this.message = a.message;
+            this.startTimer();
+        });
+    }
 
-  private clear() {
-    this.klass = 'snackbar fade-out';
-    clearInterval(this.interval);
-  }
+    private clear() {
+        this.class = 'snackbar hide';
+        clearInterval(this.interval);
+    }
 
-  private startTimer() {
-    clearInterval(this.interval);
-    this.interval = setInterval(() => {
-      this.clear();
-    }, 1000 * 10);
-  }
+    private startTimer() {
+        clearInterval(this.interval);
+        this.interval = setInterval(() => {
+            this.clear();
+        }, 1000 * 10);
+    }
 }
